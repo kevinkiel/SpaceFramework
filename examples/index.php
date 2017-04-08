e7855bfc5fa48d5b5b01a20e541b60aa07c12d70
Harwin Borger <harwin@studioborger.nl>
2017-04-08 11:37:14 +0200

    Add examples

---
+++ examples/index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+	<title>Space Framework examples</title>
+	<link rel="stylesheet" href="../dist/css/style.min.css" type="text/css">
+</head>
+<body>
+<h1>Documentation of the SpaceFramework</h1>
+<div class="clearfix">
+
+	<section class="site-margin-vertical code-container description">
+		<div class="site-position-vertical-negative space-absolute area-margin">
+
+		</div>
+		<code class="site-margin-top-negative ">.site-margin</code>
+			<h2>Example normal use of padding</h2>
+
+		<div class="site-padding area-padding code-container">
+			<code>.block-padding</code>
+			<div class="description block-padding">
+				Example of default <pre>site-object</pre>
+			</div>
+		</div>
+	</section>
+
+	<section class="area-margin site-margin-vertical code-container">
+		<code class="site-margin-top-negative ">.site-margin</code>
+		<div class="site-padding area-padding">
+			<div class="description block-padding">
+				Normal site padding
+			</div>
+		</div>
+	</section>
+
+	<!-- block object -->
+	<section class="block-margin-vertical code-container">
+		<code class="block-margin-top-negative ">.block-margin</code>
+		<div>
+			<h2 class="description">Example normal use of padding</h2>
+		</div>
+
+		<div class="block-padding area-padding code-container">
+			<code>.block-padding</code>
+			<div class="description block-padding">
+				Example of default <pre>block-object</pre>
+			</div>
+		</div>
+	</section>
+
+	<section class="block-margin-vertical code-container">
+		<code class="block-margin-top-negative">.block-margin</code>
+		<div class="block-padding area-padding">
+			<div class="description block-padding">
+				Normal site padding
+			</div>
+		</div>
+	</section>
+</div>
+
+
+</body>
+</html>
