3ab489b4b043d2be11d986da6878a9f337bdaaa9
Harwin Borger <harwin@studioborger.nl>
2017-04-20 23:42:33 +0200

    Improve documentation

---
+++ documentation/index.php
@@ -0,0 +1,144 @@
+<html>
+<head>
+	<title>Space Framework examples</title>
+	<link rel="stylesheet" href="../dist/css/style.min.css" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,600" rel="stylesheet">
+</head>
+<body>
+<div class="main-header">
+	<div class="container">
+		<div class="block-padding-vertical">
+			<h1><span class="title">SpaceFramework</span> <br>Documentation</h1>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<h2>Padding Basics</h2>
+</div>
+<div class="raster">
+	<div class="container">
+		<div class="code-container site-margin-vertical">
+			<section class="block-margin-vertical">
+				<div class="site-padding area-padding">
+					<div class="description">
+						<code>.site-padding</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+
+
+			<section class="block-margin-vertical">
+
+				<div class="site-padding-horizontal area-padding">
+					<div class="description">
+						<code>.site-padding-horizontal</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+
+			<section class="block-margin-vertical">
+				<div class="site-padding-vertical area-padding">
+					<div class="description">
+						<code>.site-padding-vertical</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+
+			<section class="block-margin-vertical">
+				<div class="site-padding-top area-padding">
+					<div class="description">
+						<code>.site-padding-top</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+
+			<section class="block-margin-vertical">
+				<div class="site-padding-right area-padding">
+					<div class="description">
+						<code>.site-padding-right</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+
+			<section class="block-margin-vertical">
+				<div class="site-padding-bottom area-padding">
+					<div class="description">
+						<code>.site-padding-bottom</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+
+
+			<section class="block-margin-vertical">
+				<div class="site-padding-left area-padding">
+					<div class="description">
+						<code>.site-padding-left</code>
+						<?php include('inc/lorem.php'); ?>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<h2>Padding Responsive</h2>
+</div>
+<div class="raster">
+	<div class="container">
+		<div class="code-container site-margin-vertical">
+			<section class="block-margin-vertical">
+				<div class="medium-site-padding-left area-padding">
+					<div class="description">
+						<code>.medium-site-padding-left</code>
+						<?php include('inc/lorem.php'); ?>
+
+					</div>
+				</div>
+			</section>
+
+			<section class="block-margin-vertical">
+				<div class="large-site-padding-left area-padding">
+					<div class="description">
+						<code>.large-site-padding-left</code>
+						<?php include('inc/lorem.php'); ?>
+
+					</div>
+				</div>
+			</section>
+
+			<section class="block-margin-vertical">
+				<div class="small-only-site-padding-left area-padding">
+					<div class="description">
+						<code>.small-only-site-padding-left</code>
+						<?php include('inc/lorem.php'); ?>
+
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<h2>Margin Negative</h2>
+</div>
+<div class="raster">
+	<div class="container">
+		<div class="code-container site-margin-vertical">
+			<section class="block-margin-vertical site-margin-horizontal-negative">
+				<div class="description">
+					<code>.site-margin-horizontal-negative</code>
+					<?php include('inc/lorem.php'); ?>
+
+				</div>
+			</section>
+		</div>
+	</div>
+</div>
+</body>
+</html>
